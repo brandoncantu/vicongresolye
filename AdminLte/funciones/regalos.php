d5180e63ce402a1495390383cbe42c9b7277c254
Brandon Cantu <brandon.2g.05@gmail.com>
2020-11-27 00:12:37 -0600

    Regalos.php config modified

--- AdminLte/funciones/regalos.php
+++ AdminLte/funciones/regalos.php
@@ -1,9 +1,9 @@
 <?php
 if($_POST['data']=='regalos'){
-    $user = 'root';
-    $password = '';
-    $db = 'vicongreso';
-    $host = '127.0.0.1';
+    $user = 'b14fa1a2e46d45';
+    $password = '116b8057';
+    $db = 'heroku_15cf40b5956ac06';
+    $host = 'us-cdbr-east-02.cleardb.com';
 
     $link = mysqli_connect($host, $user, $password, $db);
     $cantidad = array();
